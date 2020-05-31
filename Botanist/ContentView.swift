a3dfd81f82c18ddba3d7f6b1d987569e1b97ffb9
Jaden Geller <jaden.geller@gmail.com>
2020-05-30 18:24:10 -0700

    Initial Commit

---
+++ Botanist/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Botanist
+//
+//  Created by Jaden Geller on 5/30/20.
+//  Copyright © 2020 Jaden Geller. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
